9d3a701156cce20bbd9153d512949878e404cf0e
Ruslana <tretiakovaruslana7@gmail.com>
2026-04-12 19:34:42 +0200

    chore: add prisma service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { ArticleModule } from './article/article.module';
 import { CategoryModule } from './category/category.module';
 import { CommentModule } from './comment/comment.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, CategoryModule, ArticleModule, CommentModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    CategoryModule,
+    ArticleModule,
+    CommentModule,
+  ],
 })
 export class AppModule {}
